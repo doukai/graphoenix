eb6c4df86adc509a55d012da4691b98b5f511066
kai <doukai.java@gmail.com>
2021-05-08 17:37:25 +0800

    update

---
+++ src/main/java/graphql/parser/GraphqlAntlrRegister.java
@@ -0,0 +1,67 @@
+package graphql.parser;
+
+import graphql.parser.antlr.GraphqlParser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GraphqlAntlrRegister {
+
+    private final Map<String, GraphqlParser.TypeDefinitionContext> typeDefinitionContextMap = new HashMap<>();
+
+
+    public void registerDocument(GraphqlParser.DocumentContext documentContext) {
+        documentContext.definition().forEach(this::registerDefinition);
+    }
+
+    protected void registerDefinition(GraphqlParser.DefinitionContext definitionContext) {
+
+        if (definitionContext.typeSystemDefinition() != null) {
+            registerDefinition(definitionContext.typeSystemDefinition());
+        }
+    }
+
+    protected void registerDefinition(GraphqlParser.TypeSystemDefinitionContext typeSystemDefinitionContext) {
+
+        if (typeSystemDefinitionContext.typeDefinition() != null) {
+            registerDefinition(typeSystemDefinitionContext.typeDefinition());
+        }
+    }
+
+    protected void registerDefinition(GraphqlParser.TypeDefinitionContext typeDefinitionContext) {
+
+        if (typeDefinitionContext.enumTypeDefinition() != null) {
+            typeDefinitionContextMap.put(typeDefinitionContext.enumTypeDefinition().name().getText(), typeDefinitionContext);
+
+        } else if (typeDefinitionContext.objectTypeDefinition() != null) {
+            typeDefinitionContextMap.put(typeDefinitionContext.objectTypeDefinition().name().getText(), typeDefinitionContext);
+
+        } else if (typeDefinitionContext.scalarTypeDefinition() != null) {
+            typeDefinitionContextMap.put(typeDefinitionContext.scalarTypeDefinition().name().getText(), typeDefinitionContext);
+        }
+    }
+
+    public boolean exist(String name) {
+        return typeDefinitionContextMap.keySet().stream().anyMatch(key -> key.equals(name));
+    }
+
+    public String getDefinitionType(String name) {
+        final GraphqlParser.TypeDefinitionContext typeDefinitionContext = typeDefinitionContextMap.get(name);
+
+        if (typeDefinitionContext.enumTypeDefinition() != null) {
+            return typeDefinitionContext.enumTypeDefinition().ENUM().getText();
+
+        } else if (typeDefinitionContext.objectTypeDefinition() != null) {
+            return typeDefinitionContext.objectTypeDefinition().TYPE().getText();
+
+        } else if (typeDefinitionContext.scalarTypeDefinition() != null) {
+            return typeDefinitionContext.scalarTypeDefinition().SCALAR().getText();
+        }
+
+        return null;
+    }
+
+    public GraphqlParser.TypeDefinitionContext getDefinition(String name) {
+        return typeDefinitionContextMap.get(name);
+    }
+}
--- src/main/java/io/graphoenix/visitor/Test.java
+++ src/main/java/io/graphoenix/visitor/Test.java
@@ -1,19 +1,34 @@
 package io.graphoenix.visitor;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import graphql.parser.GraphqlAntlrRegister;
+import graphql.parser.GraphqlAntlrToRelationTable;
+import graphql.parser.GraphqlAntlrToTable;
 import graphql.parser.antlr.GraphqlLexer;
 import graphql.parser.antlr.GraphqlParser;
+import net.sf.jsqlparser.statement.create.table.CreateTable;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.atn.PredictionMode;
 
 import java.io.IOException;
-import java.io.UncheckedIOException;
+import java.net.URL;
+import java.util.List;
 
 public class Test {
 
-    public static void main(String[] args){
+
+    public static void main(String[] args) throws IOException {
+        GraphqlAntlrRegister graphqlAntlrRegister = new GraphqlAntlrRegister();
+        GraphqlAntlrToTable graphqlAntlrToTable = new GraphqlAntlrToTable(graphqlAntlrRegister);
+        GraphqlAntlrToRelationTable graphqlAntlrToRelationTable = new GraphqlAntlrToRelationTable(graphqlAntlrRegister);
         CodePointCharStream charStream;
 
-        charStream = CharStreams.fromString("multiSourceReader");
+
+        URL url = Resources.getResource("static/test.graphqls");
+        String sdl = Resources.toString(url, Charsets.UTF_8);
+
+        charStream = CharStreams.fromString(sdl);
         GraphqlLexer lexer = new GraphqlLexer(charStream);
 
         lexer.removeErrorListeners();
@@ -30,6 +45,14 @@ public class Test {
         parser.getInterpreter().setPredictionMode(PredictionMode.SLL);
 
         GraphqlParser.DocumentContext documentContext = parser.document();
+        graphqlAntlrRegister.registerDocument(documentContext);
+        List<CreateTable> tables = graphqlAntlrToTable.createTables(documentContext);
+        List<CreateTable> relationTables = graphqlAntlrToRelationTable.createRelationTables(documentContext);
+
+
+        tables.forEach(createTable -> System.out.println(createTable.toString()));
+        relationTables.forEach(createTable -> System.out.println(createTable.toString()));
+
 
     }
 }
