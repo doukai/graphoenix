49b570f67f24507e4c42d6f42bd100ba884b129f
kai <doukai.java@gmail.com>
2021-08-30 16:57:36 +0800

    update

---
+++ greator/src/main/java/io/graphoenix/greator/TypeExpressionInputBuilder.java
@@ -0,0 +1,21 @@
+package io.graphoenix.greator;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+import graphql.parser.antlr.GraphqlParser;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class TypeExpressionInputBuilder {
+
+//   String buildObjectExpressionInputs(List<GraphqlParser.ObjectTypeDefinitionContext> typeDefinitionContextList) throws IOException {
+//
+//      MustacheFactory mf = new DefaultMustacheFactory();
+//      Mustache mustache = mf.compile("template.mustache");
+//      mustache.execute(new PrintWriter(System.out), typeDefinitionContextList).flush();
+//
+//   }
+}
