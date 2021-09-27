efd5a90866c1cf6b5defb5738ed54ff68b5957de
kai <doukai.java@gmail.com>
2021-09-27 16:29:29 +0800

    update

--- graphoenix-mysql-translator/src/test/java/GraphqlTest.java
+++ graphoenix-mysql-translator/src/test/java/GraphqlTest.java
@@ -69,7 +69,7 @@ public class GraphqlTest {
         parser.getInterpreter().setPredictionMode(PredictionMode.SLL);
         GraphqlParser.DocumentContext documentContext = parser.document();
         graphqlAntlrManager.registerDocument(documentContext);
-        List<Select> selects = graphqlQueryToSelect.createSelects(documentContext);
+        List<Select> selects = graphqlQueryToSelect.createSelectsByQuery(documentContext);
         selects.forEach(select -> System.out.println(select.toString()));
     }
 
