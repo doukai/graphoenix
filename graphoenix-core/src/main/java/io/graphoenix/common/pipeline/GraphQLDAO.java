abaec01ffc641dac0cf158f35e6b4d3ccb39d725
Kai <doukai.java@gmail.com>
2021-12-04 17:47:25 +0800

    update

--- graphoenix-core/src/main/java/io/graphoenix/common/pipeline/GraphQLDAO.java
+++ graphoenix-core/src/main/java/io/graphoenix/common/pipeline/GraphQLDAO.java
@@ -1,62 +1,109 @@
 package io.graphoenix.common.pipeline;
 
-import com.google.auto.factory.AutoFactory;
-import com.google.gson.Gson;
-import io.graphoenix.common.pipeline.operation.OperationPipeline;
+import com.google.gson.GsonBuilder;
+import io.graphoenix.common.pipeline.operation.OperationHandler;
+import io.graphoenix.spi.dto.type.AsyncType;
+import io.graphoenix.spi.dto.type.ExecuteType;
+import io.graphoenix.spi.dto.type.OperationType;
 import io.graphoenix.spi.handler.IOperationHandler;
 import org.apache.commons.chain.impl.ChainBase;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static io.graphoenix.common.utils.ObjectCastUtil.OBJECT_CAST_UTIL;
 
-@AutoFactory
-public class GraphQLDAO extends ChainBase {
+public abstract class GraphQLDAO extends ChainBase {
 
-    private final IOperationHandler[] operationHandlers;
+    private final GsonBuilder gsonBuilder = new GsonBuilder();
 
-    public GraphQLDAO(IOperationHandler[] operationHandlers) throws Exception {
-        this.operationHandlers = operationHandlers;
+    private String query(String request, Map<String, Object> parameters) throws Exception {
+        addOperationHandlers();
+        PipelineContext pipelineContext = new PipelineContext();
+        pipelineContext.addStatus(OperationType.QUERY);
+        pipelineContext.addStatus(ExecuteType.SYNC);
+        pipelineContext.addStatus(AsyncType.OPERATION);
+        pipelineContext.add(request);
+        pipelineContext.add(parameters);
+        this.execute(pipelineContext);
+        return pipelineContext.poll(String.class);
     }
 
-    public <T> T findOne(String request, Class<T> clazz) throws Exception {
-        String json = this.createOperationPipeline().fetch(request, String.class);
-        return new Gson().fromJson(json, clazz);
+    private Mono<String> queryAsync(String request, Map<String, Object> parameters) throws Exception {
+        addOperationHandlers();
+        PipelineContext pipelineContext = new PipelineContext();
+        pipelineContext.addStatus(OperationType.QUERY);
+        pipelineContext.addStatus(ExecuteType.ASYNC);
+        pipelineContext.addStatus(AsyncType.OPERATION);
+        pipelineContext.add(request);
+        pipelineContext.add(parameters);
+        this.execute(pipelineContext);
+        return pipelineContext.pollMono(String.class);
     }
 
-    public <T> List<T> findAll(String request, Class<T> clazz) throws Exception {
-        String json = this.createOperationPipeline().fetch(request, String.class);
-        List<?> list = new Gson().fromJson(json, List.class);
+    private String mutation(String request, Map<String, Object> parameters) throws Exception {
+        PipelineContext pipelineContext = new PipelineContext();
+        pipelineContext.addStatus(OperationType.MUTATION);
+        pipelineContext.addStatus(ExecuteType.SYNC);
+        pipelineContext.addStatus(AsyncType.OPERATION);
+        pipelineContext.add(request);
+        pipelineContext.add(parameters);
+        this.execute(pipelineContext);
+        return pipelineContext.poll(String.class);
+    }
+
+    private Mono<String> mutationAsync(String request, Map<String, Object> parameters) throws Exception {
+        addOperationHandlers();
+        PipelineContext pipelineContext = new PipelineContext();
+        pipelineContext.addStatus(OperationType.MUTATION);
+        pipelineContext.addStatus(ExecuteType.ASYNC);
+        pipelineContext.addStatus(AsyncType.OPERATION);
+        pipelineContext.add(request);
+        pipelineContext.add(parameters);
+        this.execute(pipelineContext);
+        return pipelineContext.pollMono(String.class);
+    }
+
+    protected <T> T findOne(String request, Map<String, Object> parameters, Class<T> clazz) throws Exception {
+        String json = query(request, parameters);
+        return gsonBuilder.create().fromJson(json, clazz);
+    }
+
+    protected <T> List<T> findAll(String request, Map<String, Object> parameters, Class<T> clazz) throws Exception {
+        String json = query(request, parameters);
+        List<?> list = gsonBuilder.create().fromJson(json, List.class);
         return list.stream()
                 .map(item -> OBJECT_CAST_UTIL.cast(item, clazz))
                 .collect(Collectors.toList());
     }
 
-    public <T> T save(String request, Class<T> clazz) throws Exception {
-        String json = this.createOperationPipeline().fetch(request, String.class);
-        return new Gson().fromJson(json, clazz);
+    protected <T> T save(String request, Map<String, Object> parameters, Class<T> clazz) throws Exception {
+        String json = mutation(request, parameters);
+        return gsonBuilder.create().fromJson(json, clazz);
     }
 
-    public <T> Mono<T> findOneAsync(String request, Class<T> clazz) throws Exception {
-        Mono<String> stringMono = this.createOperationPipeline().fetchAsyncToMono(request, String.class);
-        return stringMono.map(json -> new Gson().fromJson(json, clazz));
+    protected <T> Mono<T> findOneAsync(String request, Map<String, Object> parameters, Class<T> clazz) throws Exception {
+        Mono<String> jsonMono = queryAsync(request, parameters);
+        return jsonMono.map(json -> gsonBuilder.create().fromJson(json, clazz));
     }
 
-    public <T> Mono<List<T>> findAllAsync(String request, Class<T> clazz) throws Exception {
-        Mono<String> stringMono = this.createOperationPipeline().fetchAsyncToMono(request, String.class);
-        return stringMono
-                .map(json -> new Gson().fromJson(json, List.class))
+    protected <T> Mono<List<T>> findAllAsync(String request, Map<String, Object> parameters, Class<T> clazz) throws Exception {
+        Mono<String> jsonMono = queryAsync(request, parameters);
+        return jsonMono
+                .map(json -> gsonBuilder.create().fromJson(json, List.class))
                 .map(list -> OBJECT_CAST_UTIL.castToList(list, clazz));
     }
 
-    public <T> Mono<T> saveAsync(String request, Class<T> clazz) throws Exception {
-        Mono<String> stringMono = this.createOperationPipeline().fetchAsyncToMono(request, String.class);
-        return stringMono.map(json -> new Gson().fromJson(json, clazz));
+    protected <T> Mono<T> saveAsync(String request, Map<String, Object> parameters, Class<T> clazz) throws Exception {
+        Mono<String> jsonMono = mutationAsync(request, parameters);
+        return jsonMono.map(json -> gsonBuilder.create().fromJson(json, clazz));
     }
 
-    private OperationPipeline createOperationPipeline() {
-        return null;
+    protected void addOperationHandler(IOperationHandler handler) {
+        addCommand(new OperationHandler(handler));
     }
+
+    protected abstract void addOperationHandlers();
 }
