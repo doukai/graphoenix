eaf02963d4aff86c6423dea5e72c077c5563417a
Kai <doukai.java@gmail.com>
2022-01-11 23:48:35 +0800

    update

--- graphoenix-core/src/main/java/io/graphoenix/core/module/DocumentManagerModule.java
+++ graphoenix-core/src/main/java/io/graphoenix/core/module/DocumentManagerModule.java
@@ -1,25 +1,9 @@
 package io.graphoenix.core.module;
 
-import io.graphoenix.core.manager.GraphQLVariablesProcessor;
+import io.graphoenix.core.manager.*;
 import io.graphoenix.spi.module.Module;
 import io.graphoenix.spi.module.Provides;
 import io.graphoenix.core.config.GraphQLConfig;
-import io.graphoenix.core.manager.GraphQLConfigRegister;
-import io.graphoenix.core.manager.GraphQLDirectiveManager;
-import io.graphoenix.core.manager.GraphQLDocumentManager;
-import io.graphoenix.core.manager.GraphQLEnumManager;
-import io.graphoenix.core.manager.GraphQLFieldManager;
-import io.graphoenix.core.manager.GraphQLFieldMapManager;
-import io.graphoenix.core.manager.GraphQLFragmentManager;
-import io.graphoenix.core.manager.GraphQLInputObjectManager;
-import io.graphoenix.core.manager.GraphQLInputValueManager;
-import io.graphoenix.core.manager.GraphQLInterfaceManager;
-import io.graphoenix.core.manager.GraphQLObjectManager;
-import io.graphoenix.core.manager.GraphQLOperationManager;
-import io.graphoenix.core.manager.GraphQLOperationRouter;
-import io.graphoenix.core.manager.GraphQLScalarManager;
-import io.graphoenix.core.manager.GraphQLSchemaManager;
-import io.graphoenix.core.manager.GraphQLUnionManager;
 import io.graphoenix.spi.antlr.IGraphQLDirectiveManager;
 import io.graphoenix.spi.antlr.IGraphQLDocumentManager;
 import io.graphoenix.spi.antlr.IGraphQLEnumManager;
@@ -157,5 +141,11 @@ public class DocumentManagerModule {
     @Singleton
     GraphQLVariablesProcessor graphQLVariablesProcessor() {
         return new GraphQLVariablesProcessor();
+    }
+
+    @Provides
+    @Singleton
+    GraphQLSelectionFilter graphQLSelectionFilter() {
+        return new GraphQLSelectionFilter(graphQLDocumentManager());
     }
 }
