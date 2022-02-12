4dce07a9954b9e66c0d51bed792a7c9d7b27c77f
Kai <doukai.java@gmail.com>
2022-02-12 10:45:04 +0800

    update

--- graphoenix-core/src/main/java/io/graphoenix/core/context/BeanContext.java
+++ graphoenix-core/src/main/java/io/graphoenix/core/context/BeanContext.java
@@ -1,5 +1,6 @@
 package io.graphoenix.core.context;
 
+import io.graphoenix.spi.context.BeanProviders;
 import io.graphoenix.spi.context.ModuleContext;
 import jakarta.inject.Provider;
 
@@ -7,7 +8,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.ServiceLoader;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -15,7 +15,7 @@ public class BeanContext {
 
     private static Set<ModuleContext> moduleContexts;
 
-    private static final Map<Class<?>, Map<String, Supplier<?>>> contextCache = new ConcurrentHashMap<>();
+    private static final ClassValue<Map<String, Supplier<?>>> CONTEXT_CACHE = new BeanProviders();
 
     static {
         moduleContexts = ServiceLoader.load(ModuleContext.class, BeanContext.class.getClassLoader()).stream()
@@ -34,12 +34,9 @@ public class BeanContext {
     }
 
     public static <T> T get(Class<T> beanClass, String name) {
-        Map<String, Supplier<?>> supplierMap = contextCache.get(beanClass);
-        if (supplierMap != null) {
-            Supplier<?> supplier = supplierMap.get(name);
-            if (supplier != null) {
-                return beanClass.cast(supplier.get());
-            }
+        Supplier<?> supplier = CONTEXT_CACHE.get(beanClass).get(name);
+        if (supplier != null) {
+            return beanClass.cast(supplier.get());
         }
         return getAndCache(beanClass, name);
     }
@@ -50,48 +47,21 @@ public class BeanContext {
 
     @SuppressWarnings("unchecked")
     public static <T> Provider<T> getProvider(Class<T> beanClass, String name) {
-        Map<String, Supplier<?>> supplierMap = contextCache.get(beanClass);
-        if (supplierMap != null) {
-            Supplier<?> supplier = supplierMap.get(name);
-            if (supplier != null) {
-                return ((Supplier<T>) supplier)::get;
-            }
+        Supplier<?> supplier = CONTEXT_CACHE.get(beanClass).get(name);
+        if (supplier != null) {
+            return ((Supplier<T>) supplier)::get;
         }
         return getAndCacheProvider(beanClass, name);
     }
 
     private static <T> T getAndCache(Class<T> beanClass, String name) {
-        Supplier<T> supplier = moduleContexts.stream()
-                .map(moduleContext -> moduleContext.getOptional(beanClass, name))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .findFirst()
-                .orElseThrow();
-
-        Map<String, Supplier<?>> supplierMap = contextCache.get(beanClass);
-        if (supplierMap == null) {
-            supplierMap = new ConcurrentHashMap<>();
-        }
-        supplierMap.put(name, supplier);
-        contextCache.put(beanClass, supplierMap);
-        return beanClass.cast(supplier.get());
+        Supplier<T> cachedSupplier = getAndCacheSupplier(beanClass, name).orElseThrow();
+        return beanClass.cast(cachedSupplier.get());
     }
 
     private static <T> Provider<T> getAndCacheProvider(Class<T> beanClass, String name) {
-        Supplier<T> supplier = moduleContexts.stream()
-                .map(moduleContext -> moduleContext.getOptional(beanClass))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .findFirst()
-                .orElseThrow();
-
-        Map<String, Supplier<?>> supplierMap = contextCache.get(beanClass);
-        if (supplierMap == null) {
-            supplierMap = new ConcurrentHashMap<>();
-        }
-        supplierMap.put(name, supplier);
-        contextCache.put(beanClass, supplierMap);
-        return supplier::get;
+        Supplier<T> cachedSupplier = getAndCacheSupplier(beanClass, name).orElseThrow();
+        return cachedSupplier::get;
     }
 
     public static <T> Optional<T> getOptional(Class<T> beanClass) {
@@ -99,32 +69,41 @@ public class BeanContext {
     }
 
     public static <T> Optional<T> getOptional(Class<T> beanClass, String name) {
-        Map<String, Supplier<?>> supplierMap = contextCache.get(beanClass);
-        if (supplierMap != null) {
-            Supplier<?> supplier = supplierMap.get(name);
-            if (supplier != null) {
-                return Optional.of(beanClass.cast(supplier.get()));
-            }
+        Supplier<?> supplier = CONTEXT_CACHE.get(beanClass).get(name);
+        if (supplier != null) {
+            return Optional.of(beanClass.cast(supplier.get()));
         }
         return getAndCacheOptional(beanClass, name);
     }
 
+    public static <T> Optional<Provider<T>> getProviderOptional(Class<T> beanClass) {
+        return getProviderOptional(beanClass, beanClass.getName());
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> Optional<Provider<T>> getProviderOptional(Class<T> beanClass, String name) {
+        Supplier<?> supplier = CONTEXT_CACHE.get(beanClass).get(name);
+        if (supplier != null) {
+            return Optional.of(((Supplier<T>) supplier)::get);
+        }
+        return getAndCacheProviderOptional(beanClass, name);
+    }
+
     private static <T> Optional<T> getAndCacheOptional(Class<T> beanClass, String name) {
-        Optional<Supplier<T>> supplierOptional = moduleContexts.stream()
-                .map(moduleContext -> moduleContext.getOptional(beanClass))
+        return getAndCacheSupplier(beanClass, name).map(Supplier::get);
+    }
+
+    private static <T> Optional<Provider<T>> getAndCacheProviderOptional(Class<T> beanClass, String name) {
+        return getAndCacheSupplier(beanClass, name).map(supplier -> ((Supplier<T>) supplier)::get);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> Optional<Supplier<T>> getAndCacheSupplier(Class<T> beanClass, String name) {
+        return moduleContexts.stream()
+                .map(moduleContext -> moduleContext.getOptional(beanClass, name))
                 .filter(Optional::isPresent)
                 .map(Optional::get)
-                .findFirst();
-
-        supplierOptional.ifPresent(supplier -> {
-                    Map<String, Supplier<?>> supplierMap = contextCache.get(beanClass);
-                    if (supplierMap == null) {
-                        supplierMap = new ConcurrentHashMap<>();
-                    }
-                    supplierMap.put(name, supplier);
-                    contextCache.put(beanClass, supplierMap);
-                }
-        );
-        return supplierOptional.map(Supplier::get);
+                .findFirst()
+                .map(supplier -> (Supplier<T>) CONTEXT_CACHE.get(beanClass).putIfAbsent(name, supplier));
     }
 }
--- graphoenix-spi/src/main/java/io/graphoenix/spi/context/BaseModuleContext.java
+++ graphoenix-spi/src/main/java/io/graphoenix/spi/context/BaseModuleContext.java
@@ -2,24 +2,18 @@ package io.graphoenix.spi.context;
 
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
 public abstract class BaseModuleContext implements ModuleContext {
 
-    private static final Map<Class<?>, Map<String, Supplier<?>>> contextMap = new ConcurrentHashMap<>();
+    private static final ClassValue<Map<String, Supplier<?>>> CONTEXT = new BeanProviders();
 
     protected static void put(Class<?> beanClass, Supplier<?> supplier) {
         put(beanClass, beanClass.getName(), supplier);
     }
 
     protected static void put(Class<?> beanClass, String name, Supplier<?> supplier) {
-        Map<String, Supplier<?>> supplierMap = contextMap.get(beanClass);
-        if (supplierMap == null) {
-            supplierMap = new ConcurrentHashMap<>();
-        }
-        supplierMap.put(name, supplier);
-        contextMap.put(beanClass, supplierMap);
+        CONTEXT.get(beanClass).put(name, supplier);
     }
 
     @Override
@@ -30,7 +24,7 @@ public abstract class BaseModuleContext implements ModuleContext {
     @SuppressWarnings("unchecked")
     @Override
     public <T> Supplier<T> get(Class<T> beanClass, String name) {
-        return (Supplier<T>) contextMap.get(beanClass).get(name);
+        return (Supplier<T>) CONTEXT.get(beanClass).get(name);
     }
 
     @Override
@@ -41,8 +35,8 @@ public abstract class BaseModuleContext implements ModuleContext {
     @SuppressWarnings("unchecked")
     @Override
     public <T> Optional<Supplier<T>> getOptional(Class<T> beanClass, String name) {
-        if (contextMap.get(beanClass) != null && contextMap.get(beanClass).get(name) != null) {
-            return Optional.of((Supplier<T>) contextMap.get(beanClass).get(name));
+        if (CONTEXT.get(beanClass).get(name) != null) {
+            return Optional.of((Supplier<T>) CONTEXT.get(beanClass).get(name));
         }
         return Optional.empty();
     }
