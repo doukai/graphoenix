927a5f73d61bdaa807ae95220f5eb06fcc779ac5
Kai <doukai.java@gmail.com>
2022-04-19 00:04:32 +0800

    Update GraphQLHttpServer.java

--- graphoenix-http-server/src/main/java/io/graphoenix/http/server/GraphQLHttpServer.java
+++ graphoenix-http-server/src/main/java/io/graphoenix/http/server/GraphQLHttpServer.java
@@ -5,6 +5,11 @@ import io.graphoenix.http.handler.GetRequestHandler;
 import io.graphoenix.http.handler.PostRequestHandler;
 import io.graphoenix.spi.handler.BootstrapHandler;
 import io.netty.channel.ChannelOption;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.cors.CorsConfig;
+import io.netty.handler.codec.http.cors.CorsConfigBuilder;
+import io.netty.handler.codec.http.cors.CorsHandler;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import reactor.netty.DisposableServer;
@@ -28,18 +33,25 @@ public class GraphQLHttpServer {
 
     public void run() {
         bootstrapHandler.bootstrap();
+        CorsConfig corsConfig =
+                CorsConfigBuilder
+                        .forAnyOrigin()
+                        .allowedRequestHeaders(HttpHeaderNames.CONTENT_TYPE)
+                        .allowedRequestMethods(HttpMethod.GET)
+                        .allowedRequestMethods(HttpMethod.POST)
+                        .build();
 
         DisposableServer server = HttpServer.create()
                 .option(ChannelOption.TCP_NODELAY, httpServerConfig.getTcpNoDelay())
                 .option(ChannelOption.SO_KEEPALIVE, httpServerConfig.getSoKeepAlive())
                 .option(ChannelOption.SO_BACKLOG, httpServerConfig.getSoBackLog())
-                .port(httpServerConfig.getPort())
+                .doOnConnection(connection -> connection.addHandlerLast("cors", new CorsHandler(corsConfig)))
                 .route(httpServerRoutes ->
                         httpServerRoutes
-                                .options(httpServerConfig.getGraphqlContextPath(), getRequestHandler::handle)
                                 .get(httpServerConfig.getGraphqlContextPath(), getRequestHandler::handle)
                                 .post(httpServerConfig.getGraphqlContextPath(), postRequestHandler::handle)
                 )
+                .port(httpServerConfig.getPort())
                 .bindNow();
 
         server.onDispose().block();
