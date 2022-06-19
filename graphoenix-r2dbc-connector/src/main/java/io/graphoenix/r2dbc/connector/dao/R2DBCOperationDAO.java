a578eba612f47a878b7548a6264ec4c1e2b77462
Kai <doukai.java@gmail.com>
2022-06-19 23:24:54 +0800

    Update R2DBCOperationDAO.java

--- graphoenix-r2dbc-connector/src/main/java/io/graphoenix/r2dbc/connector/dao/R2DBCOperationDAO.java
+++ graphoenix-r2dbc-connector/src/main/java/io/graphoenix/r2dbc/connector/dao/R2DBCOperationDAO.java
@@ -1,6 +1,7 @@
 package io.graphoenix.r2dbc.connector.dao;
 
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
@@ -12,6 +13,10 @@ import io.graphoenix.spi.dao.BaseOperationDAO;
 import reactor.core.publisher.Mono;
 
 import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Objects;
@@ -27,7 +32,22 @@ public class R2DBCOperationDAO extends BaseOperationDAO {
     private final R2dbcParameterProcessor r2dbcParameterProcessor;
 
     public R2DBCOperationDAO() {
-        this.gsonBuilder = new GsonBuilder();
+        this.gsonBuilder = new GsonBuilder()
+                .registerTypeAdapter(
+                        LocalDateTime.class,
+                        (JsonDeserializer<LocalDateTime>) (json, type, jsonDeserializationContext) ->
+                                LocalDateTime.parse(json.getAsJsonPrimitive().getAsString(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+                )
+                .registerTypeAdapter(
+                        LocalDate.class,
+                        (JsonDeserializer<LocalDate>) (json, type, jsonDeserializationContext) ->
+                                LocalDate.parse(json.getAsJsonPrimitive().getAsString(), DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+                )
+                .registerTypeAdapter(
+                        LocalTime.class,
+                        (JsonDeserializer<LocalTime>) (json, type, jsonDeserializationContext) ->
+                                LocalTime.parse(json.getAsJsonPrimitive().getAsString(), DateTimeFormatter.ofPattern("HH:mm:ss"))
+                );
         this.queryExecutor = BeanContext.get(QueryExecutor.class);
         this.mutationExecutor = BeanContext.get(MutationExecutor.class);
         this.r2dbcParameterProcessor = BeanContext.get(R2dbcParameterProcessor.class);
@@ -35,14 +55,16 @@ public class R2DBCOperationDAO extends BaseOperationDAO {
 
     @Override
     public <T> T findOne(String sql, Map<String, Object> parameters, Class<T> beanClass) {
-        JsonElement jsonElement = JsonParser.parseString(Objects.requireNonNull(queryExecutor.executeQuery(sql, r2dbcParameterProcessor.process(parameters)).block())).getAsJsonObject().get(beanClass.getTypeName().toLowerCase());
-        return gsonBuilder.create().fromJson(jsonElement, beanClass);
+        JsonElement jsonElement = JsonParser.parseString(Objects.requireNonNull(queryExecutor.executeQuery(sql, r2dbcParameterProcessor.process(parameters)).block()));
+        String key = (String) jsonElement.getAsJsonObject().keySet().toArray()[0];
+        return gsonBuilder.create().fromJson(jsonElement.getAsJsonObject().get(key), beanClass);
     }
 
     @Override
     public <T> T findAll(String sql, Map<String, Object> parameters, Type type) {
-        JsonElement jsonElement = JsonParser.parseString(Objects.requireNonNull(queryExecutor.executeQuery(sql, r2dbcParameterProcessor.process(parameters)).block())).getAsJsonObject().get(type.getTypeName().toLowerCase().concat("List"));
-        return gsonBuilder.create().fromJson(jsonElement, type);
+        JsonElement jsonElement = JsonParser.parseString(Objects.requireNonNull(queryExecutor.executeQuery(sql, r2dbcParameterProcessor.process(parameters)).block()));
+        String key = (String) jsonElement.getAsJsonObject().keySet().toArray()[0];
+        return gsonBuilder.create().fromJson(jsonElement.getAsJsonObject().get(key), type);
     }
 
     @Override
@@ -54,16 +76,18 @@ public class R2DBCOperationDAO extends BaseOperationDAO {
 
     @Override
     public <T> T save(String sql, Map<String, Object> parameters, Class<T> beanClass) {
-        JsonElement jsonElement = JsonParser.parseString(Objects.requireNonNull(mutationExecutor.executeMutations(sql, r2dbcParameterProcessor.process(parameters)).block())).getAsJsonObject().get(beanClass.getTypeName().toLowerCase());
-        return gsonBuilder.create().fromJson(jsonElement, beanClass);
+        JsonElement jsonElement = JsonParser.parseString(Objects.requireNonNull(mutationExecutor.executeMutations(sql, r2dbcParameterProcessor.process(parameters)).block()));
+        String key = (String) jsonElement.getAsJsonObject().keySet().toArray()[0];
+        return gsonBuilder.create().fromJson(jsonElement.getAsJsonObject().get(key), beanClass);
     }
 
     @Override
     public <T> Mono<T> findOneAsync(String sql, Map<String, Object> parameters, Class<T> beanClass) {
         return queryExecutor.executeQuery(sql, r2dbcParameterProcessor.process(parameters))
                 .map(json -> {
-                            JsonElement jsonElement = JsonParser.parseString(json).getAsJsonObject().get(beanClass.getTypeName().toLowerCase());
-                            return gsonBuilder.create().fromJson(jsonElement, beanClass);
+                            JsonElement jsonElement = JsonParser.parseString(json);
+                            String key = (String) jsonElement.getAsJsonObject().keySet().toArray()[0];
+                            return gsonBuilder.create().fromJson(jsonElement.getAsJsonObject().get(key), beanClass);
                         }
                 );
     }
@@ -72,8 +96,9 @@ public class R2DBCOperationDAO extends BaseOperationDAO {
     public <T> Mono<T> findAllAsync(String sql, Map<String, Object> parameters, Type type) {
         Mono<String> jsonMono = queryExecutor.executeQuery(sql, r2dbcParameterProcessor.process(parameters));
         return jsonMono.map(json -> {
-                    JsonElement jsonElement = JsonParser.parseString(json).getAsJsonObject().get(type.getTypeName().toLowerCase().concat("List"));
-                    return gsonBuilder.create().fromJson(jsonElement, type);
+                    JsonElement jsonElement = JsonParser.parseString(json);
+                    String key = (String) jsonElement.getAsJsonObject().keySet().toArray()[0];
+                    return gsonBuilder.create().fromJson(jsonElement.getAsJsonObject().get(key), type);
                 }
         );
     }
@@ -89,8 +114,9 @@ public class R2DBCOperationDAO extends BaseOperationDAO {
     public <T> Mono<T> saveAsync(String sql, Map<String, Object> parameters, Class<T> beanClass) {
         Mono<String> jsonMono = mutationExecutor.executeMutations(sql, r2dbcParameterProcessor.process(parameters));
         return jsonMono.map(json -> {
-                    JsonElement jsonElement = JsonParser.parseString(json).getAsJsonObject().get(beanClass.getTypeName().toLowerCase());
-                    return gsonBuilder.create().fromJson(jsonElement, beanClass);
+                    JsonElement jsonElement = JsonParser.parseString(json);
+                    String key = (String) jsonElement.getAsJsonObject().keySet().toArray()[0];
+                    return gsonBuilder.create().fromJson(jsonElement.getAsJsonObject().get(key), beanClass);
                 }
         );
     }
