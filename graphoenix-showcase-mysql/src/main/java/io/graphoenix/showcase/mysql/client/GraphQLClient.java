84c99f440fec4ee3b0b6b8a70d03626c695c6fd2
Kai <doukai.java@gmail.com>
2022-08-04 22:17:18 +0800

    update

--- graphoenix-showcase-mysql/src/main/java/io/graphoenix/showcase/mysql/client/GraphQLClient.java
+++ graphoenix-showcase-mysql/src/main/java/io/graphoenix/showcase/mysql/client/GraphQLClient.java
@@ -1,10 +1,18 @@
 package io.graphoenix.showcase.mysql.client;
 
+import io.graphoenix.showcase.mysql.dto.objectType.User;
 import io.graphoenix.showcase.mysql.grpc.QueryIntroTypeRequest;
 import io.graphoenix.showcase.mysql.grpc.ReactorQueryTypeServiceGrpc;
 import io.graphoenix.showcase.mysql.grpc.StringExpression;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.dataloader.BatchLoader;
+import org.dataloader.DataLoader;
+import org.dataloader.DataLoaderFactory;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 public class GraphQLClient {
 
@@ -18,6 +26,13 @@ public class GraphQLClient {
         reactorQueryTypeServiceStub.introType(QueryIntroTypeRequest.newBuilder().setName(StringExpression.newBuilder().setVal("User").build()).build())
                 .doOnSuccess(r -> System.out.println(r.getIntroType().getName()))
                 .block();
+
+//        BatchLoader<Long, User> userBatchLoader = (BatchLoader<Long, User>) userIds -> CompletableFuture.supplyAsync(() -> {
+//            return userManager.loadUsersById(userIds);
+//        });
+//
+//        DataLoader<Long, User> userLoader = DataLoaderFactory.newDataLoader(userBatchLoader);
+
 
     }
 }
