221288d3f2803fc42b5d12bc254b51fb1c67c6f1
doukai <doukai.java@gmail.com>
2022-08-05 17:07:04 +0800

    update

--- graphoenix-showcase-mysql/src/main/java/io/graphoenix/showcase/mysql/client/GraphQLClient.java
+++ graphoenix-showcase-mysql/src/main/java/io/graphoenix/showcase/mysql/client/GraphQLClient.java
@@ -1,38 +1,85 @@
 package io.graphoenix.showcase.mysql.client;
 
-import io.graphoenix.showcase.mysql.dto.objectType.User;
-import io.graphoenix.showcase.mysql.grpc.QueryIntroTypeRequest;
-import io.graphoenix.showcase.mysql.grpc.ReactorQueryTypeServiceGrpc;
-import io.graphoenix.showcase.mysql.grpc.StringExpression;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 import org.dataloader.BatchLoader;
 import org.dataloader.DataLoader;
 import org.dataloader.DataLoaderFactory;
+import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 public class GraphQLClient {
 
-    public static void main(String[] args) {
+    private static final List<User> userList = new ArrayList<>();
 
-        ManagedChannel localhost = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext()
-                .build();
+    static class User {
+        Long id;
+        String name;
+        Integer age;
+        Long bossId;
 
-        ReactorQueryTypeServiceGrpc.ReactorQueryTypeServiceStub reactorQueryTypeServiceStub = ReactorQueryTypeServiceGrpc.newReactorStub(localhost);
+        public User(Long id, String name, Integer age, Long bossId) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.bossId = bossId;
+        }
+    }
 
-        reactorQueryTypeServiceStub.introType(QueryIntroTypeRequest.newBuilder().setName(StringExpression.newBuilder().setVal("User").build()).build())
-                .doOnSuccess(r -> System.out.println(r.getIntroType().getName()))
-                .block();
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        userList.add(new User(1L, "aa", 18, 3L));
+        userList.add(new User(2L, "bb", 19, 4L));
+        userList.add(new User(3L, "cc", 20, 5L));
+        userList.add(new User(4L, "dd", 31, 5L));
+        userList.add(new User(5L, "ee", 3, 2L));
 
-//        BatchLoader<Long, User> userBatchLoader = (BatchLoader<Long, User>) userIds -> CompletableFuture.supplyAsync(() -> {
-//            return userManager.loadUsersById(userIds);
-//        });
+//        ManagedChannel localhost = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext()
+//                .build();
 //
-//        DataLoader<Long, User> userLoader = DataLoaderFactory.newDataLoader(userBatchLoader);
+//        ReactorQueryTypeServiceGrpc.ReactorQueryTypeServiceStub reactorQueryTypeServiceStub = ReactorQueryTypeServiceGrpc.newReactorStub(localhost);
+//
+//        reactorQueryTypeServiceStub.introType(QueryIntroTypeRequest.newBuilder().setName(StringExpression.newBuilder().setVal("User").build()).build())
+//                .doOnSuccess(r -> System.out.println(r.getIntroType().getName()))
+//                .block();
 
+        BatchLoader<Long, User> userBatchLoader = userIds -> CompletableFuture.supplyAsync(() -> userList.stream().filter(user -> userIds.contains(user.id)).collect(Collectors.toList()));
 
+        DataLoader<Long, User> userLoader = DataLoaderFactory.newDataLoader(userBatchLoader);
+        userLoader.load(1L)
+                .thenAccept(user -> {
+                    System.out.println("user = " + user);
+                    userLoader.load(user.bossId)
+                            .thenAccept(invitedBy -> {
+                                userLoader.load(invitedBy.bossId)
+                                        .thenAccept(invitedBy2 -> {
+                                            System.out.println("bossId = " + invitedBy2.bossId);
+                                        });
+                            });
+                });
+
+        userLoader.load(2L)
+                .thenAccept(user -> {
+                    System.out.println("user = " + user);
+                    userLoader.load(user.bossId)
+                            .thenAccept(invitedBy -> {
+                                System.out.println("bossId = " + invitedBy);
+                            });
+                });
+
+        userLoader.load(5L)
+                .thenAccept(user -> {
+                    System.out.println("user = " + user);
+                    userLoader.load(user.bossId)
+                            .thenAccept(invitedBy -> {
+                                System.out.println("bossId = " + invitedBy);
+                            });
+                });
+
+        userLoader.dispatch();
+
+        Mono.just("aaa").toFuture();
     }
 }
