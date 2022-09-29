2d93ac959ce9edf9f46e54cb5693e7333fda7a3a
Kai <doukai.java@gmail.com>
2022-09-30 00:08:17 +0800

    Update TransactionInterceptor.java

--- graphoenix-core/src/main/java/io/graphoenix/core/transaction/TransactionInterceptor.java
+++ graphoenix-core/src/main/java/io/graphoenix/core/transaction/TransactionInterceptor.java
@@ -1,11 +1,24 @@
 package io.graphoenix.core.transaction;
 
+import com.aventrix.jnanoid.jnanoid.NanoIdUtils;
+import io.r2dbc.spi.Batch;
+import io.r2dbc.spi.Connection;
 import jakarta.annotation.Priority;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Provider;
 import jakarta.interceptor.AroundInvoke;
 import jakarta.interceptor.Interceptor;
 import jakarta.interceptor.InvocationContext;
 import jakarta.transaction.Transactional;
+import org.eclipse.microprofile.reactive.streams.operators.PublisherBuilder;
+import org.reactivestreams.Publisher;
+import org.tinylog.Logger;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.context.Context;
+
+import static io.graphoenix.spi.constant.Hammurabi.TRANSACTION_ID;
 
 @ApplicationScoped
 @Transactional
@@ -13,9 +26,16 @@ import jakarta.transaction.Transactional;
 @Interceptor
 public class TransactionInterceptor {
 
+    private final Provider<Mono<Connection>> connectionProvider;
+
+    @Inject
+    public TransactionInterceptor(Provider<Mono<Connection>> connectionProvider) {
+        this.connectionProvider = connectionProvider;
+    }
+
     @SuppressWarnings("unchecked")
     @AroundInvoke
-    public Object aroundInvoke(InvocationContext invocationContext) throws NoSuchMethodException {
+    public Publisher<?> aroundInvoke(InvocationContext invocationContext) throws NoSuchMethodException {
         Transactional.TxType txType;
         Class<? extends Exception>[] rollbackOn;
         Class<? extends Exception>[] dontRollbackOn;
@@ -35,6 +55,33 @@ public class TransactionInterceptor {
         } else {
             dontRollbackOn = (Class<? extends Exception>[]) Transactional.class.getDeclaredMethod("dontRollbackOn").getDefaultValue();
         }
+
+        try {
+            if (invocationContext.getMethod().getReturnType().isAssignableFrom(Mono.class)) {
+                return Mono.usingWhen(
+                        connectionProvider.get(),
+                        connection ->
+                                Mono.from(connection.setAutoCommit(false))
+                                        .then(Mono.from(connection.beginTransaction()))
+                                        .then(Mono.from(connection.commitTransaction())),
+                        connection -> Mono.from(connection.commitTransaction()).thenEmpty(connection.close()),
+                        (connection, throwable) -> {
+                            Logger.error(throwable);
+                            return Mono.from(connection.rollbackTransaction()).thenEmpty(connection.close()).thenEmpty(Mono.error(throwable));
+                        },
+                        connection -> Mono.from(connection.rollbackTransaction()).thenEmpty(connection.close())
+                );
+            } else if (invocationContext.getMethod().getReturnType().isAssignableFrom(Flux.class)) {
+                return ((Flux<?>) invocationContext.proceed())
+                        .thenMany(connectionProvider.get().map(Connection::commitTransaction))
+                        .contextWrite(Context.of(TRANSACTION_ID, NanoIdUtils.randomNanoId()));
+            } else if (invocationContext.getMethod().getReturnType().isAssignableFrom(PublisherBuilder.class)) {
+
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
 
         switch (txType) {
             case REQUIRED:
