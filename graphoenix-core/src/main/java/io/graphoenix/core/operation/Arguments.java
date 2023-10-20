6de5d4a429e2041297e51dc60cfc4eac02ea8fa0
Kai <doukai.java@gmail.com>
2023-10-21 00:22:12 +0800

    update

--- graphoenix-core/src/main/java/io/graphoenix/core/operation/Arguments.java
+++ graphoenix-core/src/main/java/io/graphoenix/core/operation/Arguments.java
@@ -25,14 +25,14 @@ public class Arguments extends AbstractMap<String, JsonValue> implements ValueWi
         this.arguments = new LinkedHashMap<>();
     }
 
-    public Arguments(Map<String, ValueWithVariable> arguments) {
-        this.arguments = arguments;
-    }
-
     public Arguments(GraphqlParser.ArgumentsContext argumentsContext) {
         this.arguments = argumentsContext.argument().stream()
                 .map(argumentContext -> new SimpleEntry<>(argumentContext.name().getText(), ValueWithVariable.of(argumentContext.valueWithVariable())))
                 .collect(Collectors.toMap(Entry::getKey, Entry::getValue, (x, y) -> y, LinkedHashMap::new));
+    }
+
+    public Arguments(Map<?, ?> arguments) {
+        this.arguments = arguments.entrySet().stream().collect(Collectors.toMap(entry -> (String) entry.getKey(), entry -> ValueWithVariable.of(entry.getValue())));
     }
 
     public Arguments(JsonObject jsonObject) {
@@ -190,6 +190,46 @@ public class Arguments extends AbstractMap<String, JsonValue> implements ValueWi
     @Override
     public boolean isObject() {
         return true;
+    }
+
+    public static <K, V> Arguments of(K k1, V v1) {
+        return new Arguments(Map.of(k1, v1));
+    }
+
+    public static <K, V> Arguments of(K k1, V v1, K k2, V v2) {
+        return new Arguments(Map.of(k1, v1, k2, v2));
+    }
+
+    public static <K, V> Arguments of(K k1, V v1, K k2, V v2, K k3, V v3) {
+        return new Arguments(Map.of(k1, v1, k2, v2, k3, v3));
+    }
+
+    public static <K, V> Arguments of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4) {
+        return new Arguments(Map.of(k1, v1, k2, v2, k3, v3, k4, v4));
+    }
+
+    public static <K, V> Arguments of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5) {
+        return new Arguments(Map.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5));
+    }
+
+    public static <K, V> Arguments of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6) {
+        return new Arguments(Map.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6));
+    }
+
+    public static <K, V> Arguments of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7) {
+        return new Arguments(Map.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7));
+    }
+
+    public static <K, V> Arguments of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8) {
+        return new Arguments(Map.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7, k8, v8));
+    }
+
+    public static <K, V> Arguments of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9) {
+        return new Arguments(Map.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7, k8, v8, k9, v9));
+    }
+
+    public static <K, V> Arguments of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10) {
+        return new Arguments(Map.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7, k8, v8, k9, v9, k10, v10));
     }
 
     @Override
